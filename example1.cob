e417d6891d6c3a1b2a37bf827ae3987d6258a4df
Ursin Filli <git@ursinn.dev>
2020-04-28 20:02:38 +0200

    Add Example 1

---
+++ example1.cob
@@ -0,0 +1,38 @@
+       >>SOURCE FORMAT FREE
+IDENTIFICATION DIVISION.
+PROGRAM-ID. example1.
+AUTHOR. Ursin Filli
+DATE-WRITTEN. April 28th 2020
+ENVIRONMENT DIVISION.
+
+DATA DIVISION.
+FILE SECTION.
+WORKING-STORAGE SECTION.
+01 UserName PIC X(30) VALUE "You".
+01 Num1     PIC 9 VALUE ZEROS.
+01 Num2     PIC 9 VALUE ZEROS.
+01 Total    PIC 99 VALUE 0.
+01 SSNum.
+       02 SSArea   PIC 999.
+       02 SSGroup  PIC 99.
+       02 SSSerial PIC 9999.
+01 PIValue CONSTANT AS 3.14.
+
+PROCEDURE DIVISION.
+DISPLAY "What is your name " WITH NO ADVANCING
+ACCEPT UserName
+DISPLAY "Hello " UserName
+
+MOVE ZERO TO UserName
+DISPLAY UserName
+DISPLAY "Enter 2 values to sum "
+ACCEPT Num1
+ACCEPT Num2
+COMPUTE Total = Num1 + Num2
+DISPLAY Num1 " + " Num2 " = " Total
+DISPLAY "Enter your social security number "
+ACCEPT SSNum
+DISPLAY "Area " SSArea
+
+
+STOP RUN.
